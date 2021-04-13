af2499771601e9d2cf8092cd7fb6c8e79e00520c
unknown <hammad.jamil10@gmail.com>
2021-04-13 17:28:23 +0500

    second commit

--- App.js
+++ App.js
@@ -1,21 +1,69 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Pressable, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+
+  const hello = () => {
+    alert("yo")
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.scoreOXStyle}>0:0</Text>
+      <View style={styles.TicTacBoard}>
+        <View style={{flexDirection:'row'}}>
+        <Pressable onPress={hello} style={styles.PressableStyle}><Text style={styles.scoreOXStyle}>O</Text></Pressable>
+        <Pressable onPress={hello} style={styles.PressableStyle}><Text style={styles.scoreOXStyle}>X</Text></Pressable>
+        <Pressable onPress={hello} style={styles.PressableStyle}><Text style={styles.scoreOXStyle}>O</Text></Pressable>
+        </View>
+        <View style={{flexDirection:'row'}}>
+        <Pressable onPress={hello} style={styles.PressableStyle}><Text style={styles.scoreOXStyle}>X</Text></Pressable>
+        <Pressable onPress={hello} style={styles.PressableStyle}><Text style={styles.scoreOXStyle}>O</Text></Pressable>
+        <Pressable onPress={hello} style={styles.PressableStyle}><Text style={styles.scoreOXStyle}>X</Text></Pressable>
+        </View>
+        <View style={{flexDirection:'row'}}>
+        <Pressable onPress={hello} style={styles.PressableStyle}><Text style={styles.scoreOXStyle}>O</Text></Pressable>
+        <Pressable onPress={hello} style={styles.PressableStyle}><Text style={styles.scoreOXStyle}>X</Text></Pressable>
+        <Pressable onPress={hello} style={styles.PressableStyle}><Text style={styles.scoreOXStyle}>O</Text></Pressable>
+        </View>
+        
+      </View>
+
+      <Text style={styles.turn}>Player 1's Turn</Text>
+
+
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    paddingTop:35,
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#40E0D0',
+    alignItems: 'center',
+  },
+  TicTacBoard:{
+    marginTop:80,
+    justifyContent:'flex-start',
+    alignItems:'center'
+  },
+  scoreOXStyle: {
+    fontSize: 50,
+    fontWeight: 'bold',
+    color: 'white'
+  },
+  PressableStyle: {
+    backgroundColor: "#FF69B4",
+    width: 100,
+    height: 100,
     alignItems: 'center',
     justifyContent: 'center',
+    margin:1
   },
+  turn:{
+    fontSize: 25,
+    fontWeight: 'bold',
+    color: 'white'
+  }
 });
