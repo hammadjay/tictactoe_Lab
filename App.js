ca621bd79282f5c35e2225f5cb87d269d3625c31
unknown <hammad.jamil10@gmail.com>
2021-04-13 19:44:57 +0500

    fourth commit

--- App.js
+++ App.js
@@ -4,10 +4,33 @@ import { Pressable, StyleSheet, Text, View } from 'react-native';
 
 
 const Grid =["","","","","","","","",""]
+const drawGrid = [false,false,false,false,false,false,false,false,false]
+
 export default function App() {
   const [playerTurn,setPlayerTurn] = useState(true);
+  const [gameOver,setGameOver] = useState(false);
   const [symbol, setSymbol] = useState("X");
  
+  const checkWinner = () =>{
+      const winCombo = [[0,4,8],[1,4,7],[2,4,6],[0,3,6],[2,5,8],[0,1,2],[3,4,5],[6,7,8]]
+      for (let i=0 ; i<winCombo.length ; i++){
+        if(Grid[winCombo[i][0]] == symbol && Grid[winCombo[i][1]] == symbol && Grid[winCombo[i][2]] == symbol ){
+          return true
+        }
+      }
+      return false
+  }
+  const checkDraw = ()=>{
+    
+    for(let i = 0; i < Grid.length ; i++){
+      if(Grid[i].length < 1){
+        return false
+      }
+    }
+    return true;
+  }
+
+
   const checkTurn =()=>{
 
     if(playerTurn){
@@ -21,11 +44,21 @@ export default function App() {
   const checkMove = (gridNo) => {
     if(playerTurn){
       console.log(Grid[gridNo].length)
-      if(Grid[gridNo].length<1){
+      if(Grid[gridNo].length < 1){
         Grid[gridNo] = symbol
-        console.log("player 1",Grid)
-        setSymbol("O")
-        setPlayerTurn(false)
+        console.log("player 1",Grid,checkDraw())
+        
+        if(checkWinner()){
+          console.log("Player 1 won")
+        }
+        else if(checkDraw()){
+          
+          console.log("match drawn")
+        }
+        else{
+          setSymbol("O")
+          setPlayerTurn(false)
+        }
       }
       else{
         alert("illegal Move")
@@ -33,11 +66,21 @@ export default function App() {
       
     }
     else{
-      if(Grid[gridNo].length<1){
+      if(Grid[gridNo].length < 1){
         Grid[gridNo] = symbol
         console.log("player 2",Grid)
-        setSymbol("X")
-        setPlayerTurn(true)
+        if(checkWinner()){
+          console.log("Player 2 won")
+        }
+        else if(checkDraw()){
+          
+          console.log("match drawn")
+        }
+        else{
+          setSymbol("X")
+          setPlayerTurn(true)
+        }
+        
       }
       else{
         alert("illegal Move")
@@ -48,7 +91,7 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      <Text style={styles.scoreOXStyle}>0:0</Text>
+      <Text style={styles.scoreOXStyle}>0 : 0</Text>
       <View style={styles.TicTacBoard}>
         <View style={{flexDirection:'row'}}>
         <Pressable onPress={()=>{checkMove(0)}} style={styles.PressableStyle}><Text style={styles.scoreOXStyle}>{Grid[0]}</Text></Pressable>
@@ -69,9 +112,6 @@ export default function App() {
       </View>
 
         <Text style={styles.turn}>{checkTurn()}</Text>
-
-       
-
     </View>
   );
 }
