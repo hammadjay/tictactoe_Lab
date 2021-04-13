182c16f0a082fca1f1e61f0fd4d64b0cb147e57f
unknown <hammad.jamil10@gmail.com>
2021-04-13 18:18:34 +0500

    third commit

--- App.js
+++ App.js
@@ -1,36 +1,76 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState } from 'react';
 import { Pressable, StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
 
-  const hello = () => {
-    alert("yo")
+const Grid =["","","","","","","","",""]
+export default function App() {
+  const [playerTurn,setPlayerTurn] = useState(true);
+  const [symbol, setSymbol] = useState("X");
+ 
+  const checkTurn =()=>{
+
+    if(playerTurn){
+     return "Player 1's Turn"
+    }
+    else{
+      return "Player 2's Turn"
+    }
   }
+
+  const checkMove = (gridNo) => {
+    if(playerTurn){
+      console.log(Grid[gridNo].length)
+      if(Grid[gridNo].length<1){
+        Grid[gridNo] = symbol
+        console.log("player 1",Grid)
+        setSymbol("O")
+        setPlayerTurn(false)
+      }
+      else{
+        alert("illegal Move")
+      }
+      
+    }
+    else{
+      if(Grid[gridNo].length<1){
+        Grid[gridNo] = symbol
+        console.log("player 2",Grid)
+        setSymbol("X")
+        setPlayerTurn(true)
+      }
+      else{
+        alert("illegal Move")
+      }
+    }
+  }
+
+
   return (
     <View style={styles.container}>
       <Text style={styles.scoreOXStyle}>0:0</Text>
       <View style={styles.TicTacBoard}>
         <View style={{flexDirection:'row'}}>
-        <Pressable onPress={hello} style={styles.PressableStyle}><Text style={styles.scoreOXStyle}>O</Text></Pressable>
-        <Pressable onPress={hello} style={styles.PressableStyle}><Text style={styles.scoreOXStyle}>X</Text></Pressable>
-        <Pressable onPress={hello} style={styles.PressableStyle}><Text style={styles.scoreOXStyle}>O</Text></Pressable>
+        <Pressable onPress={()=>{checkMove(0)}} style={styles.PressableStyle}><Text style={styles.scoreOXStyle}>{Grid[0]}</Text></Pressable>
+        <Pressable onPress={()=>{checkMove(1)}} style={styles.PressableStyle}><Text style={styles.scoreOXStyle}>{Grid[1]}</Text></Pressable>
+        <Pressable onPress={()=>{checkMove(2)}} style={styles.PressableStyle}><Text style={styles.scoreOXStyle}>{Grid[2]}</Text></Pressable>
         </View>
         <View style={{flexDirection:'row'}}>
-        <Pressable onPress={hello} style={styles.PressableStyle}><Text style={styles.scoreOXStyle}>X</Text></Pressable>
-        <Pressable onPress={hello} style={styles.PressableStyle}><Text style={styles.scoreOXStyle}>O</Text></Pressable>
-        <Pressable onPress={hello} style={styles.PressableStyle}><Text style={styles.scoreOXStyle}>X</Text></Pressable>
+        <Pressable onPress={()=>{checkMove(3)}} style={styles.PressableStyle}><Text style={styles.scoreOXStyle}>{Grid[3]}</Text></Pressable>
+        <Pressable onPress={()=>{checkMove(4)}} style={styles.PressableStyle}><Text style={styles.scoreOXStyle}>{Grid[4]}</Text></Pressable>
+        <Pressable onPress={()=>{checkMove(5)}} style={styles.PressableStyle}><Text style={styles.scoreOXStyle}>{Grid[5]}</Text></Pressable>
         </View>
         <View style={{flexDirection:'row'}}>
-        <Pressable onPress={hello} style={styles.PressableStyle}><Text style={styles.scoreOXStyle}>O</Text></Pressable>
-        <Pressable onPress={hello} style={styles.PressableStyle}><Text style={styles.scoreOXStyle}>X</Text></Pressable>
-        <Pressable onPress={hello} style={styles.PressableStyle}><Text style={styles.scoreOXStyle}>O</Text></Pressable>
+        <Pressable onPress={()=>{checkMove(6)}} style={styles.PressableStyle}><Text style={styles.scoreOXStyle}>{Grid[6]}</Text></Pressable>
+        <Pressable onPress={()=>{checkMove(7)}} style={styles.PressableStyle}><Text style={styles.scoreOXStyle}>{Grid[7]}</Text></Pressable>
+        <Pressable onPress={()=>{checkMove(8)}} style={styles.PressableStyle}><Text style={styles.scoreOXStyle}>{Grid[8]}</Text></Pressable>
         </View>
         
       </View>
 
-      <Text style={styles.turn}>Player 1's Turn</Text>
+        <Text style={styles.turn}>{checkTurn()}</Text>
 
+       
 
     </View>
   );
@@ -64,6 +104,7 @@ const styles = StyleSheet.create({
   turn:{
     fontSize: 25,
     fontWeight: 'bold',
-    color: 'white'
+    color: 'white',
+    marginTop:10
   }
 });
